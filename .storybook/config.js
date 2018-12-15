d04961f9b9d0f122939bf4bd53413583844a1f32
Kyle Holmberg <inbox@kylemh.com>
2018-12-15 15:19:29 -0800

    Integrate a11y addon for Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
 import requireContext from './require-context.macro';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
@@ -6,5 +7,7 @@ const req = requireContext('../src/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(checkA11y);
 
 configure(loadStories, module);
