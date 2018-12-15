4e4047f723df1f8ece70857f0d5f10d0f4272113
Kyle Holmberg <inbox@kylemh.com>
2018-12-14 23:35:46 -0800

    Integrate storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,41 @@
 import { configure } from '@storybook/react';
 
+// Mock require.context()
+if (typeof require.context === 'undefined') {
+  const fs = require('fs');
+  const path = require('path');
+
+  require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) => {
+    const files = {};
+
+    function readDirectory(directory) {
+      fs.readdirSync(directory).forEach((file) => {
+        const fullPath = path.resolve(directory, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath);
+
+          return;
+        }
+
+        if (!regularExpression.test(fullPath)) return;
+
+        files[fullPath] = true;
+      });
+    }
+
+    readDirectory(path.resolve(__dirname, base));
+
+    function Module(file) {
+      return require(file);
+    }
+
+    Module.keys = () => Object.keys(files);
+
+    return Module;
+  };
+}
+
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
--- src/components/Header/__stories__/Header.stories.js
+++ src/components/Header/__stories__/Header.stories.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Header from '../Header';
 
-storiesOf('Header').add('default', () => <Header />);
+storiesOf('Header', module).add('default', () => <Header />);
