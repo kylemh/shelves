e10cecc5d512d88d9cb03d66507d36ed155cfed1
Kyle Holmberg <inbox@kylemh.com>
2018-12-15 19:22:21 -0800

    Finish <Header>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <header>
-      <h1>Shelf</h1>
+    <header className={styles.Header}>
+      <h1 className={styles.logo}>Shelf</h1>
     </header>
   )
 }
