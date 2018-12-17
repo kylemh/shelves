07ab4f0ca1b115fb649e21f975f56c37e0e12854
Kyle Holmberg <inbox@kylemh.com>
2018-12-16 18:14:35 -0800

    Use CollectionItem in Shelf component

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import CollectionItem from '../CollectionItem/CollectionItem';
+import CollectionItemPropTypes from '../CollectionItem/CollectionItemPropTypes';
 import styles from './Shelf.module.scss';
 
 class Shelf extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
     releases: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      primaryArtistName: PropTypes.string.isRequired,
-      year: PropTypes.number.isRequired,
-      id: PropTypes.number.isRequired,
+      ...CollectionItemPropTypes,
     })),
   };
 
@@ -24,10 +23,8 @@ class Shelf extends React.Component {
       <section className={styles.Shelf}>
         <h2>Shelf</h2>
         <div className={styles.itemsContainer}>
-          {props.releases.map(({ title, primaryArtistName, year, id }) => (
-              <article key={id}>
-                <p>{title} by {primaryArtistName} in {year}</p>
-              </article>
+          {props.releases.map(release => (
+              <CollectionItem {...release} key={release.id} />
             )
           )}
         </div>
