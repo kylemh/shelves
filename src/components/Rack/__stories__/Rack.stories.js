daac65f95e7d52657d379d637c00dca9ce8f8b66
Kyle Holmberg <inbox@kylemh.com>
2018-12-18 22:24:31 -0800

    Fix story for <Rack>

--- src/components/Rack/__stories__/Rack.stories.js
+++ src/components/Rack/__stories__/Rack.stories.js
@@ -3,8 +3,13 @@ import { storiesOf } from '@storybook/react';
 
 import transformReleaseData from '../../../utils/transformReleaseData';
 import mockedReleases from '../../../api/mocks/releases-0.json';
-import Rack from '../Rack';
+import { DragDropContext } from 'react-beautiful-dnd';
+import DroppableRack from '../DroppableRack';
 
 const storyData = mockedReleases.releases.slice(0, 10).map(release => transformReleaseData(release));
 
-storiesOf('Rack', module).add('default', () => <Rack collection={storyData}/>);
+storiesOf('Rack', module).add('default', () => (
+  <DragDropContext onDragEnd={() => {}}>
+    <DroppableRack collection={storyData}/>
+  </DragDropContext>
+));
